80e687f1d20fba3d99f1141e9b9a60a2128523af
Abdelhadi Omar <abdelhadioumar@gmail.com>
2023-05-22 17:16:16 +0100

    add timer and rolls count

--- src/App.js
+++ src/App.js
@@ -6,6 +6,25 @@ import Confetti from "react-confetti"
 export default function App() {
   const [dice, setDice] = React.useState(allNewDice());
   const [tenzies, setTenzies] = React.useState(false);
+  const [rolls, setRolls] = React.useState(0);
+  const [seconds, setSeconds] = React.useState(0);
+  const [started, setStarted] = React.useState(false);
+
+  // only start the timer after the user clicks the button
+  React.useEffect(() => {
+    let interval
+    if (started && !tenzies) {
+      interval = setInterval(() => {
+        setSeconds(seconds => seconds + 1);
+      }, 1000);
+      return () => clearInterval(interval);
+    }
+    if (tenzies) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [started, tenzies]);
+  
 
   React.useEffect(() => {
     const allHeld = dice.every(die => die.isHeld);
@@ -36,9 +55,16 @@ export default function App() {
     return Array.from(Array(10), () => (generateNewDie()));
   }
   function roll() {
+    setRolls(rolls + 1);
     if (tenzies) {
       setTenzies(false);
       setDice(allNewDice());
+      setRolls(0);
+      setSeconds(0);
+      setStarted(false)
+    }
+    if (!started) {
+      setStarted(!started);
     }
     else setDice(prevDice => {
       return prevDice.map(die => {
@@ -56,7 +82,11 @@ export default function App() {
   return (
     <main>
       {tenzies && <Confetti />}
-      <h1 className="title">Tenzies</h1>
+      <div className="title-box">
+        <h3>Rolls Count: <span className="rolls">{rolls}</span></h3>
+        <h1 className="title">Tenzies</h1>
+        <h3>Time: <span className="seconds">{seconds < 10 ? `0${seconds}` : `${seconds}`}</span></h3>
+      </div>
       <p className="instructions">Roll untill all dice are the same. Click each die to freeze at its current value between rolls.</p>
       <div className="dice-container">
         {diceElements}
@@ -65,7 +95,7 @@ export default function App() {
         className="roll-dice"
           onClick={roll}
         >
-          {tenzies ? 'New Game' : 'Roll'}
+          {tenzies ? 'New Game' : started ? 'Roll' : 'Start Game'}
       </button>
     </main>
   )
